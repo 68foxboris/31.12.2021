5ca9ef3fdb2c569a43df77f5856da7ed33efc719
Boris <boris.fox.solovyev@gmail.com>
2022-01-12 18:27:55 +0200

    Update UsageConfig.py

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1,6 +1,6 @@
 from Components.Harddisk import harddiskmanager
 from Components.Console import Console
-from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection, ConfigText, ConfigNumber, ConfigSet, ConfigLocations, ConfigSelectionNumber, ConfigClock, ConfigSlider, ConfigEnableDisable, ConfigSubDict, ConfigDictionarySet, ConfigInteger, ConfigPassword, ConfigIP, NoSave, ConfigBoolean
+from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection, ConfigText, ConfigNumber, ConfigSet, ConfigLocations, ConfigSelectionNumber, ConfigClock, ConfigSlider, ConfigEnableDisable, ConfigSubDict, ConfigDictionarySet, ConfigInteger, ConfigPassword, ConfigIP, NoSave, ConfigBoolean, getBoxType
 from Tools.Directories import SCOPE_HDD, SCOPE_TIMESHIFT, defaultRecordingLocation, fileContains, resolveFilename, fileHas
 from enigma import setTunerTypePriorityOrder, setPreferredTuner, setSpinnerOnOff, setEnableTtCachingOnOff, eEnv, eDVBDB, Misc_Options, eBackgroundFileEraser, eServiceEvent, eDVBLocalTimeHandler, eEPGCache
 from Components.About import GetIPsFromNetworkInterfaces
@@ -10,7 +10,10 @@ from Components.ServiceList import refreshServiceList
 from SystemInfo import SystemInfo
 from os.path import exists, islink, join as pathjoin, normpath
 import os, time, locale, skin
-from boxbranding import getDisplayType
+from boxbranding import getDisplayType, getMachineBuild, getHaveWOL, getSoCFamily
+from keyids import KEYIDS
+ 
+model = getBoxType()
 
 displaytype = getDisplayType()
 
